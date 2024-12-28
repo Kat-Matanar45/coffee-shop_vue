16f1e18907f06d5e57e6b157840e4bab51b02e01
Kat <avchimchik@gmail.com>
2024-12-28 17:17:26 +0500

    Данные из формы выводятся консоль JSON

--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -19,7 +19,7 @@
                         <div class="title mt-5">Tell us about your tastes</div>
                         <img class="beanslogo mt-5" src="@/assets/logo/Beans_logo_dark.svg" alt="Beans logo">
 
-                        <form action="#" class="mt-5">
+                        <form @submit.prevent="submit" action="#" class="mt-5">
                             <div class="form-group row">
                                 <div class="col col-12 col-sm-3 d-flex align-items-center">
                                     <label for="name-input" class="mb-0">
@@ -28,7 +28,7 @@
                                     </label>
                                 </div>
                                 <div class="col col-12 col-sm-9">
-                                    <input type="text" class="form-control" id="name-input">
+                                    <input type="text" class="form-control" id="name-input" v-model="form.name">
                                 </div>
                             </div>
 
@@ -40,7 +40,7 @@
                                     </label>
                                 </div>
                                 <div class="col col-12 col-sm-9">
-                                    <input type="email" class="form-control" id="email-input">
+                                    <input type="email" class="form-control" id="email-input" v-model="form.eMail">
                                 </div>
                             </div>
 
@@ -51,7 +51,7 @@
                                     </label>
                                 </div>
                                 <div class="col col-12 col-sm-9">
-                                    <input type="tel" class="form-control" id="phone-input">
+                                    <input type="tel" class="form-control" id="phone-input" v-model="form.phone">
                                 </div>
                             </div>
 
@@ -64,13 +64,13 @@
                                 </div>
                                 <div class="col col-12">
                                     <textarea class="form-control" name="message" id="message" rows="5"
-                                        placeholder="Leave your comments here"></textarea>
+                                        placeholder="Leave your comments here" v-model="form.text"></textarea>
                                 </div>
                             </div>
 
                             <div class="row">
                                 <div class="col">
-                                    <button class="btn btn-outline-dark send-btn">Send us</button>
+                                    <button type='submit' class="btn btn-outline-dark send-btn">Send us</button>
                                 </div>
                             </div>
                         </form>
@@ -92,7 +92,18 @@ export default {
     },
     data() {
         return {
+            form: {
+                name: '',
+                eMail: '',
+                phone: '',
+                text: ''
+            },
             titleHeader: 'Contact us'
+        }
+    },
+    methods: {
+        submit() {
+            console.log(JSON.stringify(this.form))
         }
     }
 }
